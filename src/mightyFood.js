a64acb29eb43893776b2de649ec7bfb2b6a07f43
Thiago Marinho <tgmarinho@gmail.com>
2018-08-16 19:44:51 -0400

    formating const of strong foods

---
+++ src/mightyFood.js
@@ -0,0 +1,241 @@
+
+
+export const ACELERADORES = {
+
+	category: 'CARNES E PEIXES',
+	items: [
+		{
+			name: 'Anchovas'
+		},
+		{ name: 'Arenque' },
+		{ name: 'Atum' },
+		{ name: 'Bacon' },
+		{ name: 'Camarão' },
+		{ name: 'Carangueijo' },
+		{ name: 'Carneiro' },
+		{ name: 'Carnes de Caça' },
+		{ name: 'Coelho' },
+		{ name: 'Figado de Frango' },
+		{ name: 'Figado de Vaca' },
+		{ name: 'Frango' },
+		{ name: 'Gado' },
+		{ name: 'Lagosta' },
+		{ name: 'Língua' },
+		{ name: 'Lula' },
+		{ name: 'Marisco' },
+		{ name: 'Merluza' },
+		{ name: 'Ostra' },
+		{ name: 'Pato' },
+		{ name: 'Peixes no geral' },
+		{ name: 'Peru' },
+		{ name: 'Polvo' },
+		{ name: 'Porco' },
+		{ name: 'Rins' },
+		{ name: 'Salmão' },
+		{ name: 'Sardinhas' },
+
+	]
+}
+
+
+Pepino ^ CARNES E PEIXES
+
+
+
+
+
+
+
+name: ''
+name: ''
+name: ''
+name: ''
+name: ''
+name: ''
+name: ''
+name: ''
+
+
+
+export const MODERADORES = {
+
+	category: 'Carnes e Peixes',
+	items: [
+
+		{
+			name: 'Peperoni'
+		},
+		{
+			name: 'Presunto'
+		},
+		{
+			name: 'Salame
+		}
+	]
+}
+
+}
+
+
+
+name: 'Pepino'
+
+name: 'Pimentão'
+
+name: 'Rabanete'
+
+name: 'Repolho'
+
+name: 'Rúcula'
+
+name: 'Tomates'
+
+name: 'Vagem'
+
+name: 'Zucchini'
+
+
+
+£) GORDURAS E ÓLEOS  Manteiga de amêndoas Pasta de amendoim
+f# OVOS E LATICÍNIOS
+Leite de côco(não é laticínio) Ovos(clara e gema)	GRÃOS E CEREAIS  Arroz branco
+Arroz integral
+$ BEBIDAS E ADOÇANTES	Arroz selvagem
+Água com gás(natural) Brodo(frango, peixe, gado) Café Preto	w LEGUMES E TUBÉRCULOS  Abóbora
+Café com leite / óleo de côco	Banana verde(de cozinhar)
+Café com manteiga Chá(todos)  Chá gelado(sem açúcar / adoçante)  Kefir  Suco verde(sem adoçar)	Batata doce Batata inglesa Grão de bico Inhame Mandioca  Tapioca(mandioca) Taro
+í > NOZES, SEMENTES E AFINS
+Gergelim	¥ BEBIDAS E ADOÇANTES
+Tilápia  Truta	Linhaça  Semente de abóbora	Água de côco Leite de amêndoas
+Semente de chia	Suco de fruta naturalfrecémfeito)
+& GORDURAS E ÓLEOS	Semente de girassol
+Azeite de dendê Azeite de oliva		í > NOZES, SEMENTES E AFINS  Amêndoa
+
+
+
+}
+
+
+{
+
+
+} # FRUTAS
+
+Banha de porco	Abacate	Avelã
+Ghee	Am oras	Castanha de caju
+Gordura bovina	Côco	Castanha do pará
+Gordura de pato Manteiga  Manteiga de cacao	Cranberry  Framboesa(raspberry) Limão e lima	Macadâmia Noz pecã Pistache
+Óleo de abacate Óleo de côco Óleo de macadâmia	Mirtilo(blueberry) Morango	# FRUTAS  Abacaxi  Ameixa
+Óleo de peixe	11 TEMPEROS, ERVAS E AFINS
+A r5o
+Ml ^ dl 1 dU  Alecrim	Ddl Idl Id  Caqui
+
+Abacate	Canela	Carambola
+Abobrinha	Coentro	Cereja
+Agrião	Cominho	Damasco
+Aipo  Alcachofra	Cravo  Curry	Figo  Fruta do conde
+Alface e afins	Endro	Fruta pão
+Algas  Alho  Almeirão	Gengibre  Hortelã  Maionese(caseira)	Goiaba  Grapefruit  Jaca
+Aspargo  Azeitona  Beringela  Beterraba	Manjericão Mostarda(planta) Mostarda(semente) Noz moscada	Kiwi  Laranja  Lichia  Maçã
+Brócoli	Orégano	Mamão
+Brotos  Cebola(todas)	Páprica  Pimenta(todas)	Maracujá  Melancia
+Cebolinha	Sal	Melão
+Cenoura	Salsinha	Nactarina
+Chilis	Salvia	Pêra
+Cogumelos(todos) Couve de bruxelas Couve galega Couve - de - folhas	Vinagre  Vinagra balsâmico(sem açúcar)	Pêssego  Tangerina  Uva
+Couve - flor	:= OUTROS:= OUTROS
+Ervilhas  Espinafre  Funcho	Chucrute(Sauerkraut) Pão de linhaça	Chocolate 85 % para cima
+Gengibre
+
+	^ CARNES E PEIXES
+fi GORDURAS E ÓLEOS
+w LEGUMES E TUBÉRCULOS è FRUTAS
+7 BEBIDAS E ADOÇANTES
+	* Utilize esse guia somente durante as 4 semanas do Desafio.A partir da fase 2 do Código Emagrecer de Vez, você passará a usar a Tabela de Alimentos completa dentro do portal.
+
+Material exclusivo para o CodigoEmagrecerDeVez.com.br
+Caldo de cana Cerveja
+
+Cerveja gluten - free
+
+Destilados
+
+Dextrose
+
+Energéticos(normal e diet)
+
+Erythritol
+
+Estévia
+
+Frutose
+
+Gatorade e afins Leite de soja Maltitol Mel(natural)
+
+Refrigerante(normal e diet) Sorbitol
+
+Suco de frutas industrializado
+
+Sucralose
+
+Vinho
+
+Xarope de Maple Xylitol
+Apresuntado
+
+Salsicha
+Margarina Óleo de amendoim Óleodecanola Óleo de girassol Óleo de milho
+
+Óleo de semente de algodão
+
+Óleo de soja
+
+Óleo vegetal(outros)
+Aveia
+
+Cuscus
+
+Granola
+
+Milho
+
+Quinoa
+
+Soja
+
+Trigo(em todas as formas)
+it NOZES, SEMENTES E AFINS
+Amendoim
+Feijão(todos) Lentilha
+Frutas em lata Frutas secas Geléia Tâmara Uva passa
+OVOS E LATICÍNIOS
+Cream(integral fullfat) Creme de leite Iogurte de baixa gordura Iogurte natural integral Kefir de leite Leite condensado Leite desnatado e afins Leite em pó
+
+Leite integral(não pasteurizado)
+
+Queijo Cottage
+
+Queijo de cabra
+
+Queijos
+
+Requeijão
+
+Sorvete
+
+Whey Protein
+U TEMPEROS, ERVAS E AFINS
+Caldo de carne(comprado, todos) Ketchup
+
+Maionese(comprada)
+
+Molho barbecue Molho de soja
+
+Molhos de salada industrializados Molhos industrializados Mostarda(condimento)
+Barra de cereal Barra de proteína Batata frita Chips Massas Pão(todos)
+
+Salgadinhos
+
+Salgados(coxinha, pastel, etc) Sucrilhos e cereais matinais Tortilhas
+Açúcar cristalizado Açúcar de côco Açúcar integral Açúcar mascavo Ágave Aspartame
+VELOCÍMETRO do EMAGRECIMENTO
