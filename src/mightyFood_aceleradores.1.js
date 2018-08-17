c1df7a9b7bc06c7ff40b32cdbd3424c971b54b60
Thiago Marinho <tgmarinho@gmail.com>
2018-08-16 21:15:38 -0400

    Separing in files for acelerator

--- src/mightyFood_aceleradores.1.js
+++ src/mightyFood_aceleradores.1.js
@@ -240,7 +240,6 @@ const OVOS_E_LACTICINIOS = {
 
 }
 
-
   
 const BEBIDAS_E_ADOCANTES = {
 	category: 'Bebidas e Adoçantes',
@@ -258,7 +257,6 @@ const BEBIDAS_E_ADOCANTES = {
 }
 
 
-
 const NOZES_SEMENTES_E_AFINS = {
 	category: 'Nozes, Sementes e Afins',
 	items: [
@@ -269,9 +267,6 @@ const NOZES_SEMENTES_E_AFINS = {
 		{name: 'Semente de girassol'}
 	]
 }
-
-
-
 
 const FRUTAS = {
 	category: 'Frutas',
@@ -287,9 +282,6 @@ const FRUTAS = {
 
 	]
 }
-
-
-
 
 
 const TEMPEROS_ERVAS_E_AFINS = {
@@ -318,11 +310,6 @@ const TEMPEROS_ERVAS_E_AFINS = {
 		{name: 'Vinagra balâmico (sem açucar)'},
 	]
 }
- 
-
-  
-
-
 
 
 const OUTROS = {
@@ -339,271 +326,3 @@ const OUTROS = {
 }
 
 
-
-// MODERADORES
-
-
-const XX = {
-	category: '',
-	items: [
-		{
-			name: ''
-		},
-
-	]
-}
-
-
-const XX = {
-	category: '',
-	items: [
-		{name: ''},
-		{name: ''},
-		{name: ''},
-		{name: ''},
-		{name: ''},
-		{name: ''},
-		{name: ''},
-		{name: ''},
-		{name: ''},
-		{name: ''},
-		{name: ''},
-
-	]
-}
-
-
-const CARNES_E_PEIXES = {
-	category: 'Carnes e Peixes',
-	items: [
-		{name: 'Peperoni'},
-		{name: 'Presunto'},
-		{name: 'Salame'},
-
-	]
-}
-
-const GORDURAS_E_OLEOS = {
-	category: 'Gorudas e Óleos',
-	items: [
-		{name: 'Manteiga de am�ndoas'},
-		{name: 'Pasta de amendoim'}
-
-	]
-}
-
-
-const GRAOS_E_CEREAIS = {
-	category: 'Grãos e Cereais',
-	items: [
-		{name: 'Arroz branco'},
-		{name: 'Arroz integral'},
-		{name: 'Arroz selvagem'},
-	
-	]
-}
-
-    
-const LEGUMES_E_TUBERCULOS = {
-	category: 'Legurmes e Tubérculos',
-	items: [
-		{name: ''},
-		{name: ''},
-		{name: ''},
-		{name: ''},
-		{name: ''},
-		{name: ''},
-		{name: ''},
-		{name: ''},
-		{name: ''},
-		{name: ''},
-		{name: ''},
-
-	]
-}
-
-w   Ab�bora
-Banana verde (de cozinhar)
-Batata doce Batata inglesa Gr�o de bico Inhame Mandioca  Tapioca (mandioca) Taro
-
-
-� BEBIDAS E ADO�ANTES
-�gua de c�co Leite de am�ndoas
-Suco de fruta naturalfrec�mfeito)
-
-�> NOZES, SEMENTES E AFINS  Am�ndoa
-
-
-Avel�
-Castanha de caju
-Castanha do par�
-Macad�mia Noz pec� Pistache
-# FRUTAS  Abacaxi  Ameixa
-
-
-Ddl Idl Id  Caqui
-
-Carambola
-Cereja
-Damasco
-Figo  Fruta do conde
-Fruta p�o
-Goiaba  Grapefruit  Jaca
-Kiwi  Laranja  Lichia  Ma��
-Mam�o
-Maracuj�  Melancia
-Mel�o
-Nactarina
-P�ra
-P�ssego  Tangerina  Uva
-:= OUTROS
-Chocolate 85% para cima
-
-
-
-
-
-
-
-
-carnes e Peixes
-Apresuntado
-
-Salsicha
-
-
-
-Gordura e �leos
-
-Margarina �leo de amendoim �leodecanola �leo de girassol �leo de milho
-
-�leo de semente de algod�o
-
-�leo de soja
-
-�leo vegetal (outros)
-Aveia
-
-Cuscus
-
-Granola
-
-Milho
-
-Quinoa
-
-Soja
-
-Trigo (em todas as formas)
-
-
-
-
-
-
-
-
-
-
-
-legurmes e fruta
-
-
-Feij�o (todos) Lentilha
-
-
-
-ovos e laticinios
-
-Cream (integral fullfat) Creme de leite Iogurte de baixa gordura Iogurte natural integral Kefir de leite Leite condensado Leite desnatado e afins Leite em p�
-
-Leite integral (n�o pasteurizado)
-
-Queijo Cottage
-
-Queijo de cabra
-
-Queijos
-
-Requeij�o
-
-Sorvete
-
-Whey Protein
-
-
-
-
-bebidas e adocantes
-
-A��car cristalizado A��car de c�co A��car integral A��car mascavo �gave Aspartame
-
-Caldo de cana Cerveja
-
-Cerveja gluten-free
-
-Destilados
-
-Dextrose
-
-Energ�ticos (normal e diet)
-
-Erythritol
-
-Est�via
-
-Frutose
-
-Gatorade e afins Leite de soja Maltitol Mel (natural)
-
-Refrigerante (normal e diet) Sorbitol
-
-Suco de frutas industrializado
-
-Sucralose
-
-Vinho
-
-Xarope de Maple Xylitol
-
-
-
-
-
-
-nozes e afins
-
-Amendoim
-
-
-
-Furutas
-
-Frutas em lata Frutas secas Gel�ia T�mara Uva passa
-
-
-temperos e ervas
-
-Caldo de carne (comprado,todos) Ketchup
-
-Maionese (comprada)
-
-Molho barbecue Molho de soja
-
-Molhos de salada industrializados Molhos industrializados Mostarda (condimento)
-
-
-
-
-
-
-
-
-cereais
-
-
-Barra de cereal Barra de prote�na Batata frita Chips Massas P�o (todos)
-
-Salgadinhos
-
-Salgados (coxinha, pastel, etc) Sucrilhos e cereais matinais Tortilhas
