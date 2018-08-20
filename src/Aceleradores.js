050556687556b61214e17ad13cfef0eefe76654b
Thiago Marinho <tgmarinho@gmail.com>
2018-08-20 19:59:27 -0400

    até aqui deu certo

--- src/Aceleradores.js
+++ src/Aceleradores.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { ListItem, ListItemText, Checkbox, Paper } from '@material-ui/core';
+
 import ACELERADORES from './mightyFood/mightyFoodAceleradores'
 
 const Aceleradores = () => {
 
     return (
         <div>
+
+            <div>{ACELERADORES.CARNES_E_PEIXES.category}</div>
+
             {
                 ACELERADORES.CARNES_E_PEIXES.items.map((item, index) => (
-                    <p key={index} >
-                        {item.name}
-                    </p>
+                    <Paper className="food-acelerator">
+                        <ListItem>
+                            <ListItemText
+                                key={index}
+                                primary={item.name}>
+                            </ListItemText>
+                        </ListItem>
+                    </Paper>
                 ))
             }
         </div>
