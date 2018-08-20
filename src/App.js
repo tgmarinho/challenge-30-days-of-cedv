74a55e1f240379c4ee69eab0aa2489500ef097cd
Thiago Marinho <tgmarinho@gmail.com>
2018-08-20 16:15:12 -0400

    tentativa de usar menu do task

--- src/App.js
+++ src/App.js
@@ -1,44 +1,104 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import logo from "./logo.svg";
+import "./App.scss";
 // import { ACELERADORES } from './mightyFood/mightyFoodAceleradores';
 // import { MODERADORES } from './mightyFood/mightyFoodModeradores';
-import { RETARDADORES } from './mightyFood/mightyFoodRetardadores';
+import { RETARDADORES } from "./mightyFood/mightyFoodRetardadores";
+import { withStyles } from '@material-ui/core/styles';
+import { MenuContainer } from './menu/MenuContainer';
+import { ToolbarComponent } from './menu/toolbar/ToolbarComponent';
 
+const drawerWidth = 240;
 
-
-
-
-
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    height: 440,
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 36,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing.unit * 7,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing.unit * 9,
+    },
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+  },
+});
 
 class App extends Component {
   render() {
-
-    for ( let i in RETARDADORES) {
-      console.log(RETARDADORES[i].category)
+    for (let i in RETARDADORES) {
+      console.log(RETARDADORES[i].category);
     }
 
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-
-
-
-
-
-        {
-
-  RETARDADORES.CARNES_E_PEIXES.items.map((item, index) => (
-            <p key={index} className="App-intro">
-              {item.name}
-            </p>
-          ))
-
-        }
-
+      <div className="root">
+        <Fragment>
+          <CssBaseline />
+          <ToolbarComponent />
+          <MenuContainer />
+        </Fragment>
+        {<div className="content" >
+          <div className="toolbar" >
+            {/* <Routes /> */}
+            {RETARDADORES.CARNES_E_PEIXES.items.map((item, index) => (
+              <p key={index} >
+                {item.name}
+              </p>
+            ))}
+          </div>
+        </div>}
 
 
 
@@ -47,4 +107,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withStyles(styles, { withTheme: true })(App);
---
+++ src/menu/Menu.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Drawer from "@material-ui/core/Drawer";
+import { default as Person } from "@material-ui/icons/Person";
+import { default as Home } from "@material-ui/icons/Home";
+import List, {
+  ListItem,
+  ListItemIcon,
+  ListItemText
+} from "@material-ui/icons/List";
+
+export const Menu = ({ open, toggleMenu }) => (
+  <Drawer open={open} onClose={toggleMenu(true)}>
+    <List className="menu-list">
+      <div>
+        <Link to="/home" style={{ textDecoration: "none" }}>
+          <ListItem button onClick={toggleMenu(true)}>
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItem>
+        </Link>
+        <Link to="/profile" style={{ textDecoration: "none" }}>
+          <ListItem button onClick={toggleMenu(true)}>
+            <ListItemIcon>
+              <Person />
+            </ListItemIcon>
+            <ListItemText primary="Perfil" />
+          </ListItem>
+        </Link>
+      </div>
+    </List>
+  </Drawer>
+);
